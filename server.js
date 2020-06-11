b6b4177bcbf58244c64cc5b6c45e7dac7e0e974b
Paulinely Morgan <paulinelym@gmail.com>
2020-06-11 20:38:37 -0300

    mudança rota

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const TwitterController = require('./src/controllers/TwitterController')
 app.get('/perfil/:perfil', TwitterController.getTweets)
 app.get('/coletar', TwitterController.coletarFintwit)
 app.get('/mostrar/:perfil?', TwitterController.mostrarFintwit)
-app.get('/frequencia/:frase', TwitterController.listaFrequencia)
+app.post('/frequencia', TwitterController.listaFrequencia)
 
 app.listen(port, () => {
     console.log(`On PORT ${port}!`)
