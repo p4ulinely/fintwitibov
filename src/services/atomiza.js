470938708e2b92121ffbcc714b750e2aa55f9e11
Paulinely Morgan <paulinelym@gmail.com>
2020-06-11 20:50:17 -0300

    melhoria atomizador - para eliminar urls, ponto e 2 pontos

--- src/services/atomiza.js
+++ src/services/atomiza.js
@@ -35,26 +35,30 @@ const listaMaisFrequentes = str => {
     return tokensUnicosFrequencia
 }
 
-// metodo para atomizar frases em palavras
+// metodo para atomizar frases em palavras e suas respectivas frequencias
 const atomizar = frase => {
 
-    const proibidos = [';', ',', '', '(', ')', '{', '}', '[', ']']
-    let temp = []
+    const proibidos = [';', ',', '.', ':', '(', ')', '{', '}', '[', ']']
+    let palavrasComFrequencias = []
     
-    frase.split(' ').forEach(palavra => {
-        palavra = palavra.toLowerCase()
+    frase = frase.split(' ')
+
+    for(let i = 0; i < frase.length; i++){
+
+        palavra = frase[i].toLowerCase()
+        
+        if(eUmaURL(palavra)) continue // elimina urls
 
         // elimina caracteres proibidos
         let charsFiltrados = palavra.split('').filter(char => ( 
             !proibidos.includes(char)
         ))
-
         let palavraFiltrada = charsFiltrados.join('')
-        if(eUmaURL(palavraFiltrada)) palavraFiltrada = "" // elimina urls
-        if(palavraFiltrada != "") temp.push(palavraFiltrada)
-    })
+
+        if(palavraFiltrada != "") palavrasComFrequencias.push(palavraFiltrada)
+    }
     
-    return temp
+    return palavrasComFrequencias
 }
 
 // metodo para verificar se string é URL ou não
@@ -62,7 +66,7 @@ const eUmaURL = str => {
 
     let eRegular = new RegExp(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi)
 
-    return (str.match(eRegular)) ? true : false
+    return str.match(eRegular) ? true : false
 }
 
 exports.frequencia = listaMaisFrequentes
